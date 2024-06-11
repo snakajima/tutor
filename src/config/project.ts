690e700103d4fe2ec3c586b420e461a29406c4dd
snakajima <satoshi.nakajima@gmail.com>
2024-06-11 16:29:13 -0700

    ai-tutor

--- src/config/project.ts
+++ src/config/project.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAd-pX35BsCvIBDXstf_LmQaz82PNFGcMI",
-  authDomain: "fir-vue-startup-kit.firebaseapp.com",
-  databaseURL: "https://fir-vue-startup-kit.firebaseio.com",
-  projectId: "fir-vue-startup-kit",
-  storageBucket: "fir-vue-startup-kit.appspot.com",
-  messagingSenderId: "225404938985",
-  appId: "1:225404938985:web:a2cea9fab1722fce",
-  messageKey: "BGwIKr3yZdXmg4Pl-BpIQX_ETtHYl0RqvAJFoc-hYKFyaoU3wZXarqto5DSq1cemFuARMjFvJ02B83T5ga42MKo",
+  apiKey: "AIzaSyDgy4O3u3ikqx6mvO9xzGFuhp56i8leExU",
+  authDomain: "ai-tango.firebaseapp.com",
+  projectId: "ai-tango",
+  storageBucket: "ai-tango.appspot.com",
+  messagingSenderId: "20670388878",
+  appId: "1:20670388878:web:ebcc640368f708e65fc7df",
+  measurementId: "G-ZBMX85SNJ2"
 };
