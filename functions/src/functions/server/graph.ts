7bacd8243641aa782df6b990a2209e70044511c5
snakajima <satoshi.nakajima@gmail.com>
2024-06-12 13:24:32 -0700

    result

--- functions/src/functions/server/graph.ts
+++ functions/src/functions/server/graph.ts
@@ -13,43 +13,112 @@ if (!admin.apps.length) {
 
 const db = admin.firestore();
 
+export const graph_template = {
+  version: 0.3,
+  nodes: {
+    apiKey: {
+      value: functions.config().openai_api.key
+    },
+    meaning_llm: {
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+        apiKey: ":apiKey",
+        system: "You are a dictionary writer. Write the meaning of the given word.",
+      },
+      inputs: { prompt: ":word" },
+    },
+    meaning: {
+      agent: "copyAgent",
+      isResult: true,
+      inputs: [":meaning_llm.choices.$0.message.content"],
+    },
+    meaning_jp_llm: {
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+        apiKey: ":apiKey",
+        system: "あなたは英語の教師です。与えられた単語の意味を日本語で説明してください。",
+      },
+      inputs: { prompt: ":word" },
+    },
+    meaning_jp: {
+      agent: "copyAgent",
+      isResult: true,
+      inputs: [":meaning_jp_llm.choices.$0.message.content"],
+    },
+    samples_llm: {
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+        apiKey: ":apiKey",
+        system:
+          "与えられた単語を含む、英語の文章を10個作って、日本語に訳して。あまり難しい単語は使わずに。フォーマットはJSONで、以下のフォーマットで。\n" +
+          "```json\n[{en:'Hello.', jp:'こんにちは。']\n```",
+      },
+      inputs: { prompt: ":word" },
+    },
+    samples: {
+      agent: "jsonParserAgent",
+      isResult: true,
+      inputs: [":samples_llm.choices.$0.message.content"],
+    },
+    similar_llm: {
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+        apiKey: ":apiKey",
+        system:
+          "与えられた英単語をと類似するいくつか英単語を並べて、日本語で違いを説明して。フォーマットはJSONで、以下のフォーマットで。\n" +
+          "```json\n[{word:'Awesome.', jp:'本当に素晴らしいことを強調したい時に使います。']\n```",
+      },
+      inputs: { prompt: ":word" },
+    },
+    similar: {
+      agent: "jsonParserAgent",
+      isResult: true,
+      inputs: [":similar_llm.choices.$0.message.content"],
+    },
+    root_llm: {
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+        apiKey: ":apiKey",
+        system: "あなたは英語の教師です。与えられた単語の語源を日本語で解説して。",
+      },
+      inputs: { prompt: ":word" },
+    },
+    root: {
+      agent: "copyAgent",
+      isResult: true,
+      inputs: [":root_llm.choices.$0.message.content"],
+    },
+  },
+};
+
 export const onWordCreate = async (snapshot: functions.firestore.QueryDocumentSnapshot, context: functions.EventContext<{
   word: string;
 }>) => {
   const { word } = context.params;
   console.log("onCreate", snapshot.data(), word);
-  const doc = db.doc(`/words/${word}/samples/1`);
-  await doc.create({
-    message: `Hello World, ${word}`,
-  });
+  const doc = db.doc(`/words/${word}`);
+
+  const graph_data:any = Object.assign({}, graph_template);
+  graph_data.nodes.word = { value: word };
+  const graph = new GraphAI(graph_data, agents);
+  const result = await graph.run();
+  await doc.update({
+    result
+  })
 };
 
 export const query = async (req: express.Request, res: express.Response) => {
   const { word } = req.params;
   const doc = db.doc(`/words/${word}`);
   await doc.create({
-    message: "Hello",
-    word: word,
+    word,
   });
-
-  const graphdata: GraphData = {
-    version: 0.5,
-    nodes: {
-      query: {
-        agent: "openAIAgent",
-        params: {
-          model: "gpt-4o",
-          apiKey: functions.config().openai_api.key,
-          // baseURL: "https://api.openai.com/v1/chat/completions",
-        },
-        isResult: true,
-        inputs: { prompt: "Hello" },
-      },
-    }
-  }
-  const graph = new GraphAI(graphdata, agents);
-  const result = await graph.run();  
-  res.json(result);
+  res.json({ success:true, word });
 };
 
 export const test = async (req: express.Request, res: express.Response) => {
