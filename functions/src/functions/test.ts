3086ee30fd52c5d159060a2a2f2c976b76af8052
snakajima <satoshi.nakajima@gmail.com>
2024-06-11 17:00:56 -0700

    graphai

--- functions/src/functions/test.ts
+++ functions/src/functions/test.ts
@@ -1,10 +1,23 @@
 import * as functions from "firebase-functions";
+import { GraphAI, GraphData } from "graphai";
+import * as agents from "@graphai/agents";
 
-export const test = async (
-  data: {},
-  context: functions.https.CallableContext
-) => {
+export const test = async (data: {}, context: functions.https.CallableContext) => {
   const uid = context?.auth?.uid;
   console.log(uid, data);
-  return {};
+  const graphdata: GraphData = {
+    nodes: {
+      source: {
+        value: "Hello World"
+      },
+      result: {
+        agent: "sleeperAgent",
+        inputs: [":source"],
+        isResult: true,
+      }
+    }
+  }
+  const graph = new GraphAI(graphdata, agents);
+  const result = await graph.run();  
+  return result;
 };
