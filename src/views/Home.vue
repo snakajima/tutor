74fa224a5efbeb151f4e769d587e65c0219c4d2a
snakajima <satoshi.nakajima@gmail.com>
2024-06-13 07:27:25 -0700

    arrow_down

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
     <div class="basis-3/4 text-left">
       <div v-if="selectedWord" class="m-1 ml-2">
         <div class="text-3xl">{{ selectedWord.word }}</div>
-        <div class="mt-2 font-bold">意味：英語<span class="material-icons">face</span></div>
+        <div class="mt-2 font-bold">意味：英語<span class="material-icons">keyboard_arrow_down</span></div>
         <div class="ml-2" v-html="md.render(selectedWord.result.meaning)" />
         <div class="mt-2 font-bold">意味：日本語</div>
         <div class="ml-2" v-html="md.render(selectedWord.result.meaning_jp)" />
