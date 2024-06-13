31828dfbae830a90aed1d314ccab800cdc1b7855
snakajima <satoshi.nakajima@gmail.com>
2024-06-12 21:35:57 -0700

    words

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,38 @@
 <template>
-  <div class="home">
-    <div class="flex items-center justify-center space-x-8">
-      Hello
+  <div class="flex flex-row">
+    <div class="basis-1/4 bg-indigo-500 text-white text-lg">
+      <div>word</div>
+    </div>
+    <div class="basis-3/4">
+      contents
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { getFunctions, httpsCallable } from "firebase/functions";
+import { firebaseConfig } from "../config/project";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import "firebase/firestore";
+import { collection, doc, setDoc, getDocs } from "firebase/firestore"; 
 
-const functions = getFunctions(undefined, "");
-const testFunction = httpsCallable(functions, 'test');
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
 export default defineComponent({
   name: "HomePage",
   components: {},
   setup() {
-    const test = async () => {
-      const result = await testFunction();
-      console.log(result);
-    }
-    test();
+    const words = collection(db, "words");
+    const foo = async () => {
+      const querySnapshot = await getDocs(words);
+      querySnapshot.forEach((doc:any) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id);
+      });
+    };
+    foo();
     return {}
   }
 });
