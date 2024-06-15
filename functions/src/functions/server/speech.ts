6af313e5478ecb132485dca20d10d5865261eea4
snakajima <satoshi.nakajima@gmail.com>
2024-06-15 06:00:04 -0700

    uuid

--- functions/src/functions/server/speech.ts
+++ functions/src/functions/server/speech.ts
@@ -1,4 +1,5 @@
 import OpenAI from "openai";
+import { v4 as uuidv4 } from "uuid";
 import * as admin from "firebase-admin";
 import * as express from "express";
 const { getStorage, getDownloadURL } = require("firebase-admin/storage");
@@ -16,12 +17,13 @@ const openai = new OpenAI({
 });
 
 const sound = async (fileName: string, input: string) => {
-  const mp3 = await openai.audio.speech.create({
+  const audio = await openai.audio.speech.create({
     model: "tts-1",
     voice: "shimmer",
+    // response_format: "aac",
     input,
   });
-  const buffer = Buffer.from(await mp3.arrayBuffer());
+  const buffer = Buffer.from(await audio.arrayBuffer());
   console.log(`sound generated: ${input}, ${buffer.length}`);
 
   const bucket = storage.bucket("ai-tango.appspot.com");
@@ -32,6 +34,7 @@ const sound = async (fileName: string, input: string) => {
 
 export const generate = async (req: express.Request, res: express.Response) => {
   const word = req.params.word.toLowerCase();
-  const url = await sound(`words/${word}.mp3`, word);
+  const uniqueId = uuidv4();
+  const url = await sound(`words/${uniqueId}.mp3`, word);
   res.json({ success:true, word, url });
 }
