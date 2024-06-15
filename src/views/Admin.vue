437adcaae228338e0655c8632e03279709bca23e
snakajima <satoshi.nakajima@gmail.com>
2024-06-14 20:05:11 -0700

    onSnapshot on admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,14 +1,13 @@
 <template>
-  <div>admin2</div>
   <div v-for="word in words" :key="word">
-    {{ word }}
+    {{ word.word }}
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, onUnmounted, ref } from "vue";
 import { firebaseConfig } from "../config/project";
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getDocs, getFirestore } from "firebase/firestore";
 import "firebase/firestore";
 import { collection, doc, getDoc, onSnapshot } from "firebase/firestore";
 import "@material-design-icons/font/filled.css";
@@ -18,17 +17,18 @@ const db = getFirestore();
 
 export default defineComponent({
   setup() {
-    const words = ref<Array<string>>([]);
+    const words = ref<Array<any>>([]);
     const refWords = collection(db, "words");
-    const openBook = async (bookId: string) => {
-      const refDoc = doc(db, `/books/${bookId}`);
-      const docBook = await getDoc(refDoc);
-      const data = docBook.data();
-      if (data) {
-        words.value = data.words;
-      }
-    };
-    openBook("book1");
+    const unsub = onSnapshot(refWords, (collection) => {
+      const array:Array<any> = [];
+        collection.forEach((doc) => {
+        array.push(doc.data());
+      });
+      words.value = array;
+    });
+    onUnmounted(() => {
+      unsub();
+    });
 
     return {
       words
