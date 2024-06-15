2e98e9467fa2777f1152f7223cd28692b3a07484
snakajima <satoshi.nakajima@gmail.com>
2024-06-15 09:42:38 -0700

    sound for word

--- functions/src/functions/server/speech.ts
+++ functions/src/functions/server/speech.ts
@@ -11,6 +11,7 @@ if (!admin.apps.length) {
   admin.initializeApp();
 }
 const storage = getStorage();
+const db = admin.firestore();
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
@@ -34,7 +35,23 @@ const sound = async (fileName: string, input: string) => {
 
 export const generate = async (req: express.Request, res: express.Response) => {
   const word = req.params.word.toLowerCase();
+  const docRef = db.doc(`/words/${word}`);
+  const doc = await docRef.get();
+  const data = doc.data();
+  if (!data) {
+    res.json({ success:false, reason:`invalid word: ${word}` });
+    return;
+  }
+  if (data.speach) {
+    res.json({ success:false, reason:`already have: ${data.speach}` });
+    return;
+  }
+
   const uniqueId = uuidv4();
   const url = await sound(`words/${uniqueId}.mp3`, word);
+  await docRef.update({
+    speach: url,    
+  })
+
   res.json({ success:true, word, url });
 }
