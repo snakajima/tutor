ffb566bce9d53de4e02a6d857799459b2db61f4b
snakajima <satoshi.nakajima@gmail.com>
2024-06-16 14:43:04 -0700

    toeic 600 to 900

---
+++ functions/src/functions/server/toeic.ts
@@ -0,0 +1,26 @@
+const toeic600 = ["accommodate", "accounting", "actually", "additional", "administration", "advance", "advertising", "afford", "analyst", "annual", "appear", "applicant", "appreciate", "architect", "assess", "attend", "attract", "benefit", "budget", "cater", "colleague", "compare", "complicated", "concern", "conference", "consumer", "contract", "current", "decide", "delay", "delighted", "department", "designate", "develop", "donate", "eliminate", "encourage", "enroll", "estate", "exceed", "except", "executive", "exhibit", "expect", "expertise", "explain", "facility", "fail", "familiar", "feature", "fee", "figure", "frequent", "further", "handle", "identification", "improve", "include", "inquire", "instantly", "insurance", "invest", "invite", "invoice", "involve", "luggage", "manufacture", "miss", "notice", "offer", "owe", "participate", "passenger", "plenty", "preservation", "previous", "proceed", "promote", "prosperous", "provide", "publicity", "recommend", "refund", "remind", "remove", "replacement", "representative", "reservation", "responsible", "result", "review", "severe", "shipment", "shortage", "store", "survey", "transportation", "voucher", "workplace", "workshop"];
+
+const toeic700 = ["acquisition", "admit", "agenda", "allocate", "annex", "appliance", "appropriately", "approximately", "aspect", "auditorium", "bid", "brochure", "candidate", "cardboard", "checkup", "commend", "component", "conduct", "cuisine", "deficit", "demand", "designate", "discard", "durable", "eligible", "embarrassing", "enthusiastic", "exclusive", "expedite", "expire", "extinguish", "facilitate", "hallway", "handout", "inventory", "justify", "lottery", "novice", "obliged", "outdated", "oval", "pedestrian", "plumber", "pottery", "premise", "procedure", "prohibit", "property", "pursue", "reference", "reimburse", "relief", "retrieve", "shopper", "slate", "souvenir", "speculate", "standstill", "stroll", "subsidiary", "substitute", "superb", "superintendent", "supplement", "surpass", "surrounding", "suspicion", "swiftly", "tariff", "thaw", "timepiece", "token", "tolerate", "transfer", "transparent", "tuition", "turbulent", "turnout", "unanimous", "unearth", "uniformity", "utensil", "utilization", "vacant", "valid", "vaporize", "vendor", "venue", "vice", "virtue", "warehouse", "warning", "warrant", "welfare", "wheelbarrow", "wilderness", "withstand", "zeal", "zealous", "zenith"];
+
+const toeic800 = [
+    "abridged", "accomplished", "acknowledgement", "aging", "alert", "allergy",
+    "appetizer", "applause", "assorted", "attire", "austere", "avid", "ban",
+    "bear", "boost", "breathtakingly", "bulk", "burst", "bypass", "carrier",
+    "certified", "chronic", "circulation", "cite", "colossal", "commerce",
+    "compliance", "component", "confidential", "consistent", "correspondent",
+    "courteous", "craven", "culinary", "deposit", "distinctive", "distinguished",
+    "drowsiness", "enthusiasm", "entrepreneur", "exceptional", "exonerate",
+    "exponentially", "fabulous", "faculty", "former", "fragile", "halt", "hectic",
+    "implement", "inclement", "informative", "inherit", "initiative", "inn",
+    "incidence", "intermission", "introductory", "itinerary", "leading", "locksmith",
+    "lure", "malfunction", "mortgage", "noteworthy", "nursery", "outlay", "overhaul",
+    "paralegal", "patent", "patronage", "peer", "physician", "plush", "prescription",
+    "proceed", "proofreader", "posture", "query", "regulate", "regimen", "resume",
+    "revenue", "seasoned", "skyrocket", "spare", "stagnant", "stunning", "surgical",
+    "tidy", "trim", "typo", "unwilling", "valuable", "verification", "veterinarian",
+    "waive", "welder", "wildlife", "wring"
+  ];
+
+  const toeic900 = ["abash", "abhor", "abide", "abysmal", "acupuncture", "adjourn", "afflict", "affront", "aggrieved", "algae", "allude", "amble", "annihilate", "anorexia", "bleak", "blunder", "bounty", "brass", "bulimia", "burgeoning", "bustle", "canopy", "capricious", "caricature", "chagrin", "chalice", "decease", "deciduous", "defamation", "default", "delve", "desolate", "engrossed", "entail", "envisage", "excrete", "exodus", "feign", "fiasco", "flamboyant", "fleet", "flunk", "forbear", "garnish", "grandiose", "gratifying", "graze", "growl", "haughty", "haze", "hermit", "hindsight", "hinge", "hoax", "ignoble", "imposing", "inept", "infatuated", "insurrection", "juxtapose", "kudos", "latch", "latitude", "mar", "mnemonic", "morbid", "nimbus", "nirvana", "obliterate", "omnivorous", "optical", "outlandish", "pandemonium", "perturb", "placid", "presumptuous", "privy", "prodigal", "profusely", "pseudonym", "puke", "ramshackle", "ransack", "recoil", "redeem", "replenish", "rudimentary", "satirical", "savory", "savvy", "scaffold", "shun", "singular", "tattered", "trample", "upright", "venerate", "vermin", "viable", "wrath"];
+
+  
