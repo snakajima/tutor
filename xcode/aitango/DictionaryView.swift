ce5e5b505918024955fda3e2ed782aebe7ae4454
snakajima <satoshi.nakajima@gmail.com>
2024-06-23 05:04:58 -0700

    scroll

--- xcode/aitango/DictionaryView.swift
+++ xcode/aitango/DictionaryView.swift
@@ -24,101 +24,103 @@ struct DictionaryView: View {
         self.model = WordModel(word: word, path: path)
     }
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            switch model.state {
-            case .idle:
-                Color.clear.onAppear(perform: {
-                    model.load()
-                })
-            case .loading:
-                Text("loading")
-            case .nodata:
-                Text("No Data").onAppear() {
-                    model.generate()
-                }
-            case .generating:
-                Text("Generating...")
-            case .loaded:
-                if let samples = model.samples {
-                    Button("例文") {
-                        isSamplesVisible.toggle()
-                    }.font(. system(size: 24))
-                    if (isSamplesVisible) {
-                        ForEach(Array(samples.enumerated()), id: \.offset) { index, sample in
-                            HStack {
-                                Text(sample.en)
-                                Spacer()
-                                if let voice = sample.voice {
-                                    Button("", systemImage: "speaker.wave.3.fill") {
-                                        if let url = URL(string: voice) {
-                                            player = AVPlayer(url: url)
-                                            guard let player else { return }
-                                            player.play()
-                                        }
-                                    }.font(. system(size: 24))
-                                } else {
-                                    Button("", systemImage: "speaker.wave.2.fill") {
-                                        model.generateSampleVoice(index: index)
-                                    }.font(. system(size: 24))
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                switch model.state {
+                case .idle:
+                    Color.clear.onAppear(perform: {
+                        model.load()
+                    })
+                case .loading:
+                    Text("loading")
+                case .nodata:
+                    Text("No Data").onAppear() {
+                        model.generate()
+                    }
+                case .generating:
+                    Text("Generating...")
+                case .loaded:
+                    if let samples = model.samples {
+                        Button("例文") {
+                            isSamplesVisible.toggle()
+                        }.font(. system(size: 24))
+                        if (isSamplesVisible) {
+                            ForEach(Array(samples.enumerated()), id: \.offset) { index, sample in
+                                HStack {
+                                    Text(sample.en)
+                                    Spacer()
+                                    if let voice = sample.voice {
+                                        Button("", systemImage: "speaker.wave.3.fill") {
+                                            if let url = URL(string: voice) {
+                                                player = AVPlayer(url: url)
+                                                guard let player else { return }
+                                                player.play()
+                                            }
+                                        }.font(. system(size: 24))
+                                    } else {
+                                        Button("", systemImage: "speaker.wave.2.fill") {
+                                            model.generateSampleVoice(index: index)
+                                        }.font(. system(size: 24))
+                                    }
                                 }
                             }
                         }
                     }
-                }
-                if ((model.meaning) != nil) {
-                    HStack {
-                        Button("意味（英語）") {
-                            isMeaningVisible.toggle()
+                    if ((model.meaning) != nil) {
+                        HStack {
+                            Button("意味（英語）") {
+                                isMeaningVisible.toggle()
+                            }.font(. system(size: 24))
+                            Spacer()
+                        }
+                        if (isMeaningVisible) {
+                            Text(model.meaning!)
+                        }
+                    }
+                    if ((model.meaning_jp) != nil) {
+                        Button("意味（日本語）") {
+                            isMeaningJPVisible.toggle()
                         }.font(. system(size: 24))
-                        Spacer()
+                        if (isMeaningJPVisible) {
+                            Text(model.meaning_jp!)
+                        }
                     }
-                    if (isMeaningVisible) {
-                        Text(model.meaning!)
-                    }
-                }
-                if ((model.meaning_jp) != nil) {
-                    Button("意味（日本語）") {
-                        isMeaningJPVisible.toggle()
-                    }.font(. system(size: 24))
-                    if (isMeaningJPVisible) {
-                        Text(model.meaning_jp!)
-                    }
-                }
-                if let similar = model.similar {
-                    Button("同義語") {
-                        isSimilarVisible.toggle()
-                    }.font(. system(size: 24))
-                    if (isSimilarVisible) {
-                        ForEach(Array(similar.enumerated()), id: \.offset) { index, sample in
-                            HStack {
-                                Text("**\(sample.en)**").frame(width: 100, alignment: .leading)
-                                Text(sample.jp)
+                    if let similar = model.similar {
+                        Button("同義語") {
+                            isSimilarVisible.toggle()
+                        }.font(. system(size: 24))
+                        if (isSimilarVisible) {
+                            ForEach(Array(similar.enumerated()), id: \.offset) { index, sample in
+                                HStack {
+                                    Text("**\(sample.en)**").frame(width: 100, alignment: .leading)
+                                    Text(sample.jp)
+                                }
                             }
                         }
                     }
-                }
-                if let antonym = model.antonym {
-                    Button("反対語") {
-                        isAntonymVisible.toggle()
-                    }.font(. system(size: 24))
-                    if (isAntonymVisible) {
-                        ForEach(Array(antonym.enumerated()), id: \.offset) { index, sample in
-                            HStack {
-                                Text("**\(sample.en)**").frame(width: 100, alignment: .leading)
-                                Text(sample.jp)
+                    if let antonym = model.antonym {
+                        Button("反対語") {
+                            isAntonymVisible.toggle()
+                        }.font(. system(size: 24))
+                        if (isAntonymVisible) {
+                            ForEach(Array(antonym.enumerated()), id: \.offset) { index, sample in
+                                HStack {
+                                    Text("**\(sample.en)**").frame(width: 100, alignment: .leading)
+                                    Text(sample.jp)
+                                }
                             }
                         }
                     }
-                }
-                if let story = model.story {
-                    Button("ストーリー") {
-                        isStoryVisible.toggle()
-                    }.font(. system(size: 24))
-                    if (isStoryVisible) {
-                        Text(story)
+                    if let story = model.story {
+                        Button("ストーリー") {
+                            isStoryVisible.toggle()
+                        }.font(. system(size: 24))
+                        if (isStoryVisible) {
+                            Text(story)
+                        }
                     }
+                    Spacer()
                 }
-                Spacer()
             }
         }
     }
