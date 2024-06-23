b1035ff7beb9b9a7e0630f03830bc2d8c59e53e3
snakajima <satoshi.nakajima@gmail.com>
2024-06-22 20:40:29 -0700

    BookModel

--- xcode/aitango/ContentView.swift
+++ xcode/aitango/ContentView.swift
@@ -12,32 +12,7 @@ import SwiftData
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-struct Book: Hashable {
-    var id: String
-    var title: String
-    var words: [String]
-}
 
-@Observable class BooksModel {
-    public var books: [Book] = []
-    private var db = Firestore.firestore()
-    
-    init() {
-        db.collection("books")
-            .addSnapshotListener { [weak self] querySnapshot, error in
-                guard let self else { return }
-                guard let documents = querySnapshot?.documents else {
-                    print("No documents found")
-                    return
-                }
-                self.books = documents.compactMap { snapshot -> Book in
-                    let document = snapshot.data()
-                    print("doc=", document["title"] ?? "N/A", snapshot.documentID)
-                    return Book(id: snapshot.documentID, title: document["title"] as! String, words: document["words"] as! [String])
-                }
-            }
-    }
-}
 
 @Observable class WordModel {
     enum State {
