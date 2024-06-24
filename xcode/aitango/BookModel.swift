f9d43eecb1faf0e4baebdbe7171404724f7a6214
snakajima <satoshi.nakajima@gmail.com>
2024-06-24 16:58:41 -0700

    BookModel.getItem

--- xcode/aitango/BookModel.swift
+++ xcode/aitango/BookModel.swift
@@ -19,4 +19,23 @@ final class BookModel {
         self.bookId = bookId
         self.wordItem = wordItem
     }
+    
+    public static func getItem(modelContext: ModelContext, bookId: String, wordItem: WordItem) -> BookModel? {
+        let id = "\(bookId)/\(wordItem.id)"
+        let predicate = #Predicate<BookModel> { $0.id == id }
+        let descriptor = FetchDescriptor<BookModel>(predicate: predicate)
+        do {
+            let bookModels = try modelContext.fetch(descriptor)
+            if let bookModel = bookModels.first {
+                return bookModel
+            }
+            print("BookModel:inserting", id)
+            let bookModel = BookModel(bookId: bookId, wordItem: wordItem)
+            modelContext.insert(bookModel)
+            return bookModel
+        } catch {
+            print("BookModel.getItem failed \(id)")
+            return nil
+        }
+    }
 }
