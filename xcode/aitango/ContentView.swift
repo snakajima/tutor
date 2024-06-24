082bbb964aee23f4ed2dce2d51de463574a3ff85
snakajima <satoshi.nakajima@gmail.com>
2024-06-24 16:34:00 -0700

    WordLinkView

--- xcode/aitango/ContentView.swift
+++ xcode/aitango/ContentView.swift
@@ -12,32 +12,7 @@ import SwiftData
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-struct WordLinkView: View {
-    @Environment(\.modelContext) private var modelContext
-    private let word: String
-    private let bookId: String
-    @State private var wordItem = WordItem(word: "dummy")
 
-    init(word: String, bookId: String) {
-        self.word = word
-        self.bookId = bookId
-    }
-
-    var body: some View {
-        NavigationLink {
-            DictionaryView(word: word, path: "register/" + bookId + "/" + word)
-            .navigationTitle(word)
-            .padding([.leading, .trailing], 10)
-        } label: {
-            HStack {
-                Circle().fill(wordItem.level.color()).frame(width:24, height:24)
-                Text(word)
-            }.onAppear() {
-                wordItem = WordItem.getItem(modelContext: modelContext, word: word) ?? wordItem
-            }
-        }
-    }
-}
 
 struct ContentView: View {
     
---
+++ xcode/aitango/WordLinkView.swift
@@ -0,0 +1,36 @@
+//
+//  WordLinkView.swift
+//  aitango
+//
+//  Created by SATOSHI NAKAJIMA on 6/24/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct WordLinkView: View {
+    @Environment(\.modelContext) private var modelContext
+    private let word: String
+    private let bookId: String
+    @State private var wordItem = WordItem(word: "dummy")
+
+    init(word: String, bookId: String) {
+        self.word = word
+        self.bookId = bookId
+    }
+
+    var body: some View {
+        NavigationLink {
+            DictionaryView(word: word, path: "register/" + bookId + "/" + word)
+            .navigationTitle(word)
+            .padding([.leading, .trailing], 10)
+        } label: {
+            HStack {
+                Circle().fill(wordItem.level.color()).frame(width:24, height:24)
+                Text(word)
+            }.onAppear() {
+                wordItem = WordItem.getItem(modelContext: modelContext, word: word) ?? wordItem
+            }
+        }
+    }
+}
