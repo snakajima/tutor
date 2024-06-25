e6107469d9108a7c9e88dc9b4b8ee35aa678da86
snakajima <satoshi.nakajima@gmail.com>
2024-06-25 05:49:52 -0700

    toeic.ts

---
+++ books/toeic.ts
@@ -0,0 +1,146 @@
+const toeic400c = [
+    "survey", "interview", "exhibition", "conference", "available", "annual", "equipment", "department", "refund", "receipt",
+    "schedule", "meeting", "customer", "employee", "actually", "according to", "change", "contact", "purchase", "provide",
+    "building", "probably", "delay", "opportunity", "agree", "invoice", "appointment", "increase", "piece", "participate in",
+    "collect", "once", "currently", "process", "problem", "experience", "tour", "while", "center", "supervisor", "suggestion",
+    "coupon", "receive", "participant", "note that", "confirm", "meet", "reduce", "concern", "way", "special", "prepare",
+    "renovation", "early", "view", "return", "approve", "budget", "team", "recently", "shop", "fine", "home", "reservation",
+    "additional", "medical", "move", "resource", "show", "design", "total", "presentation", "bring", "improve", "several",
+    "news", "service", "through", "until", "passenger", "produce", "allow", "representative", "check", "leave", "issue",
+    "price", "last", "promote", "hold", "idea", "industry", "feature", "replacement", "pay", "update", "director", "hire",
+    "floor", "course", "museum", "recommend", "great", "install", "member", "size", "management", "review", "sincerely",
+    "logo", "revise", "corporate", "construction", "deadline", "sign", "replace", "find", "register", "merchandise", "important",
+    "open", "facility", "identification", "instead", "before", "remember", "public", "submit", "vacation", "guest", "award",
+    "discuss", "cost", "wait", "entrance", "program", "read", "choose", "however", "free", "ticket", "expand", "application",
+    "item", "résumé", "vehicle", "park", "council", "book", "candidate", "complete", "discount", "quality", "payment", "arrange",
+    "different", "project", "few", "after", "subject", "already", "require", "due", "consider", "ship", "possible", "local",
+    "area", "into", "delivery", "professional", "buy", "carry", "belong", "type", "since", "colleague", "request", "material",
+    "fee", "each", "benefit", "furniture", "again", "set", "during", "sorry", "financial", "be interested in", "attend",
+    "maintenance", "supply", "welcome", "conduct", "report", "begin", "whether", "upcoming", "purpose", "research", "large",
+    "encourage", "finish", "send", "charge", "decision", "inventory", "publish", "think", "reception", "display", "both",
+    "advance", "always", "plan", "ready", "visit", "document", "successful", "class", "library", "seminar", "sell", "close",
+    "expect", "arrive", "remove", "position", "talk", "describe", "flight", "repair", "training", "a lot of", "learn", "unfortunately",
+    "people", "cancel", "include", "location", "client", "place", "travel", "though", "board", "write", "feedback", "performance",
+    "soon", "per"
+  ];
+
+  const toeic500c = [
+    "catering", "ensure", "estimate", "potential", "remind", "intend", "delighted", "despite",
+    "specialize in", "complimentary", "although", "beverage", "honor", "mayor", "venue",
+    "immediately", "sound", "contemporary", "badge", "appliance", "assist", "inspection",
+    "expire", "necessary", "associate", "paperwork", "Excuse me.", "Let’s", "remain", "correct",
+    "continue", "certainly", "fabric", "patient", "applicant", "architect", "attach", "variety",
+    "anyway", "conversation", "forget", "shuttle", "appear", "crew", "vote", "busy", "coworker",
+    "excellent", "grocery", "save", "cause", "attract", "launch", "status", "procedure",
+    "invitation", "lobby", "realize", "effort", "mainly", "campaign", "key", "proposal",
+    "greet", "negotiate", "quantity", "behind", "comfortable", "expensive", "head", "appreciate",
+    "branch", "investment", "deposit", "difficult", "temporary", "protection", "wish", "film",
+    "condition", "device", "personnel", "professor", "complaint", "multiple", "exchange",
+    "manage", "favorite", "quite", "distance", "luggage", "admission", "inquire", "ingredient",
+    "technique", "attention", "consultant", "postpone", "revenue", "speech", "highly", "term",
+    "agenda", "demonstration", "competition", "specific", "rather", "impress", "fact",
+    "signature", "payroll", "acknowledge", "win", "seem", "trouble", "convention", "task",
+    "without", "reschedule", "teach", "handle", "distribute", "suitable", "between",
+    "throughout", "choice", "official", "region", "notify", "exercise", "brochure", "recognize",
+    "similar", "usual", "artwork", "secure", "lease", "excite", "damage", "environment",
+    "except", "famous", "nearly", "renew", "manual", "exceed", "worry", "mind", "figure",
+    "analysis", "search", "ability", "ideal", "industrial", "advise", "hard", "transfer", "expo",
+    "landscape", "initiative", "dining", "laboratory", "traditional", "accommodate", "title",
+    "further", "vendor", "specialist", "raise", "spend", "consumer", "gain", "various", "often",
+    "convenient", "wear", "previous", "fix", "standard", "solution", "else", "understand", "tier",
+    "together", "committee", "congratulations", "extend", "avoid", "strategy", "inform",
+    "especially", "mistake", "commercial", "therefore", "recruit", "topic", "contain",
+    "organization", "responsible", "bill", "either", "prefer", "interior", "involve", "executive",
+    "traffic", "assign", "far from", "warranty", "seek", "nearby", "effective", "permit",
+    "attempt", "example", "properly", "base", "clear", "under", "response", "surprise",
+    "verify", "better", "reliable", "neither"
+  ];
+  
+
+const toeic500b = [
+    "conference", "provide", "department", "opportunity", "schedule", 
+    "deadline", "applicant", "conversation", "ingredient", "grocery", 
+    "famous", "comfortable", "worry", "luggage", "consumer", 
+    "ability", "responsible", "gain", "performance", "solution", 
+    "client", "understand", "document", "soon", "library", 
+    "vehicle", "despite", "résumé", "continue", "potential", 
+    "attach", "catering", "appear", "complimentary", "appreciate", 
+    "necessary", "protection", "paperwork", "personnel", "greet"
+];
+
+const toeic600a = ["accommodate", "accounting", "actually", "additional", "administration", "advance", "advertising", "afford", "analyst", "annual", "appear", "applicant", "appreciate", "architect", "assess", "attend", "attract", "benefit", "budget", "cater", "colleague", "compare", "complicated", "concern", "conference", "consumer", "contract", "current", "decide", "delay", "delighted", "department", "designate", "develop", "donate", "eliminate", "encourage", "enroll", "estate", "exceed", "except", "executive", "exhibit", "expect", "expertise", "explain", "facility", "fail", "familiar", "feature", "fee", "figure", "frequent", "further", "handle", "identification", "improve", "include", "inquire", "instantly", "insurance", "invest", "invite", "invoice", "involve", "luggage", "manufacture", "miss", "notice", "offer", "owe", "participate", "passenger", "plenty", "preservation", "previous", "proceed", "promote", "prosperous", "provide", "publicity", "recommend", "refund", "remind", "remove", "replacement", "representative", "reservation", "responsible", "result", "review", "severe", "shipment", "shortage", "store", "survey", "transportation", "voucher", "workplace", "workshop"];
+
+const toeic600b = ["itinerary", "reservation", "questionnaire", "refund", "administration", "luggage", "advertising", "inquire", "annual", "frequent", "secretary", "adjust", "definitely", "district", "affordable", "administrative", "directory", "donate", "otherwise", "undergo", "worth", "emphasize", "voucher", "durable", "store", "cashier", "shortage", "nutrition", "representative", "patron", "invoice", "identification", "cuisine", "insurance", "instrument", "recommend", "accomplishment", "budget", "blueprint", "replacement"];
+
+const toeic600c = [
+    "subscribe", "questionnaire", "itinerary", "degree", "certificate", "enclose", "deal with", "highlight",
+    "neighborhood", "prior to", "significant", "prescription", "eatery", "be supposed to", "eligible", "I’m afraid…",
+    "broadcast", "definitely", "implement", "on behalf of", "finally", "exclusive", "occur", "predict", "terminal",
+    "in the meantime", "affordable", "ceiling", "major", "directory", "regularly", "otherwise", "depart", "introduction",
+    "impact", "worth", "decrease", "beyond", "whenever", "willing", "college", "showcase", "formal", "adjust",
+    "relevant", "absolutely", "district", "maximum", "cuisine", "yield", "approximate", "clerk", "fund", "decline",
+    "autograph", "particular", "uniform", "apologize", "accessible", "instrument", "photocopy", "administrative",
+    "contribute", "digest", "classic", "equally", "enroll", "achievement", "in writing", "donate", "decade",
+    "temperature", "reflect", "owe", "normally", "audience", "practical", "hurry", "unique", "guarantee", "undergo",
+    "institute", "hardly", "outstanding", "aside from", "inside", "baggage", "method", "in a row", "profile",
+    "emphasize", "handbook", "satisfaction", "defect", "moment", "foundation", "clarify", "auditorium", "exit",
+    "recipient", "progress", "decorate", "guess", "unless", "efficiency", "ladder", "quick", "workplace", "durable",
+    "merger", "priority", "primary", "input", "monitor", "entire", "structure", "individual", "finalize", "evidence",
+    "stack", "qualify", "regulation", "summary", "compact", "constant", "gather", "shortly", "incorporate",
+    "projector", "panel", "layout", "fare", "determine", "accomplishment", "simple", "aware", "tenant", "pleasure",
+    "demand", "blueprint", "advantage", "dental", "metro", "overhead", "waterproof", "language", "perfect", "above",
+    "luck", "exactly", "influence", "urgent", "valuable", "assess", "cashier", "dedicate", "earn", "from scratch",
+    "a couple of", "social", "connect", "plenty of", "overnight", "clearance", "onto", "sponsorship", "flexible",
+    "automatically", "alone", "nutrition", "familiar", "patron", "entry", "broad", "steady", "transaction", "delicious",
+    "checkout", "wheelbarrow", "explore", "promise", "anticipate", "cafeteria", "society", "anniversary", "evaluation",
+    "handout", "direct", "appropriate", "common", "general", "whole", "proud", "compare", "numerous", "networking",
+    "acquire", "establish", "overseas", "discover", "envelope", "newly", "expose", "means", "quarter", "internship",
+    "walkway", "medication", "format", "conservation", "below", "profit", "volume", "restore", "satisfy", "knowledge",
+    "compliment", "oversee", "judge", "retire", "huge", "frequent", "initial", "expert", "approach"
+  ];
+
+const toeic700a = ["acquisition", "admit", "agenda", "allocate", "annex", "appliance", "appropriately", "approximately", "aspect", "auditorium", "bid", "brochure", "candidate", "cardboard", "checkup", "commend", "component", "conduct", "cuisine", "deficit", "demand", "designate", "discard", "durable", "eligible", "embarrassing", "enthusiastic", "exclusive", "expedite", "expire", "extinguish", "facilitate", "hallway", "handout", "inventory", "justify", "lottery", "novice", "obliged", "outdated", "oval", "pedestrian", "plumber", "pottery", "premise", "procedure", "prohibit", "property", "pursue", "reference", "reimburse", "relief", "retrieve", "shopper", "slate", "souvenir", "speculate", "standstill", "stroll", "subsidiary", "substitute", "superb", "superintendent", "supplement", "surpass", "surrounding", "suspicion", "swiftly", "tariff", "thaw", "timepiece", "token", "tolerate", "transfer", "transparent", "tuition", "turbulent", "turnout", "unanimous", "unearth", "uniformity", "utensil", "utilization", "vacant", "valid", "vaporize", "vendor", "venue", "vice", "virtue", "warehouse", "warning", "warrant", "welfare", "wheelbarrow", "wilderness", "withstand", "zeal", "zealous", "zenith"];
+const toeic700b = ["zealous", "warehouse", "warrant", "speculate", "souvenir", "inventory", "facilitate", "refreshment", "expire", "municipal", "tuition", "deserve", "venue", "keynote", "represent", "illustrate", "vacancy", "destination", "feasible", "allocate", "brochure", "reputation", "designate", "curb", "enthusiastic", "awful", "stroll", "vendor", "approximately", "coincidence", "conduct", "encounter", "pedestrian", "complicated", "domestic", "crucial", "biography", "surpass", "devote", "exclusive"];
+
+const toeic700c = ["refreshment", "incredible", "furthermore", "municipal", "overview", "sufficient", "refill", "talent", "aspect", "nevertheless", "tuition", "persuade", "regardless of", "comprehensive", "eliminate", "enhance", "perhaps", "premise", "remark", "scheme", "generate", "conclude", "adjacent to", "likewise", "prospect", "capability", "enormous", "checkup", "substitute", "failure", "massive", "enlarge", "moderate", "reject", "interruption", "fluctuate", "generous", "consecutive", "intermediate", "aisle", "deserve", "sightseeing", "readability", "meanwhile", "majority", "postal", "voice", "foreign", "fieldwork", "checklist", "keynote", "minimum", "expertise", "flaw", "token", "outcome", "enterprise", "prevent", "scholarship", "import", "amazing", "fortunate", "illustrate", "urgency", "hesitate", "represent", "surpass", "destination", "behavior", "proficiency", "function", "recreation", "recall", "beforehand", "downstairs", "sustain", "translate", "transcript", "confident", "overcome", "anymore", "utility", "allocate", "whereas", "accustom", "forecast", "resign", "reasonable", "incline", "spokesperson", "utensil", "accurate", "bid", "prototype", "fiscal", "spectator", "inconvenience", "income", "situation", "moreover", "fantastic", "detour", "amateur", "genre", "thus", "tend to do", "overflow", "browse", "validate", "examine", "quote", "reward", "typical", "guidebook", "mission", "average", "asset", "usage", "respectively", "instant", "archive", "vacancy", "enthusiastic", "mere", "seldom", "workforce", "occupy", "competence", "opposite", "utilize", "intensive", "insight", "skip", "explanation", "devote", "discontinue", "truly", "kickoff", "entertainment", "neutral", "biography", "precise", "factor", "conform", "eventually", "reputation", "curb", "innovative", "malfunction", "crucial", "prerequisite", "reluctant", "principle", "perceive", "cooperation", "controversial", "reinforce", "worldwide", "empty", "fundamental", "heritage", "sensitive", "execute", "gradually", "declare", "beside", "struggle", "domestic", "duration", "complicated", "maintain", "origin", "infrastructure", "dramatic", "engagement", "calculate", "installment", "population", "manuscript", "imagine", "encounter", "milestone", "concept", "reimburse", "freelance", "useful", "excessive", "absence", "reply", "shortage", "scope", "landlord", "background", "bargain", "invaluable", "remote", "cite", "coincidence", "rapid", "feasible"];
+
+
+const toeic800a = [
+    "abridged", "accomplished", "acknowledgement", "aging", "alert", "allergy",
+    "appetizer", "applause", "assorted", "attire", "austere", "avid", "ban",
+    "bear", "boost", "breathtakingly", "bulk", "burst", "bypass", "carrier",
+    "certified", "chronic", "circulation", "cite", "colossal", "commerce",
+    "compliance", "component", "confidential", "consistent", "correspondent",
+    "courteous", "craven", "culinary", "deposit", "distinctive", "distinguished",
+    "drowsiness", "enthusiasm", "entrepreneur", "exceptional", "exonerate",
+    "exponentially", "fabulous", "faculty", "former", "fragile", "halt", "hectic",
+    "implement", "inclement", "informative", "inherit", "initiative", "inn",
+    "incidence", "intermission", "introductory", "itinerary", "leading", "locksmith",
+    "lure", "malfunction", "mortgage", "noteworthy", "nursery", "outlay", "overhaul",
+    "paralegal", "patent", "patronage", "peer", "physician", "plush", "prescription",
+    "proceed", "proofreader", "posture", "query", "regulate", "regimen", "resume",
+    "revenue", "seasoned", "skyrocket", "spare", "stagnant", "stunning", "surgical",
+    "tidy", "trim", "typo", "unwilling", "valuable", "verification", "veterinarian",
+    "waive", "welder", "wildlife", "wring"
+  ];
+
+  const toeic800b = [
+    "entrepreneur", "faculty", "mandatory", "applause", "bulk", "specification", "adapt", 
+    "obstacle", "adopt", "nominate", "welder", "trim", "stunning", "revenue", 
+    "proofreader", "posture", "patent", "inn", "halt", "distinguished", "carrier", 
+    "courteous", "admire", "correspondent", "tailor", "allergy", "steep", "duplicate", 
+    "circulation", "ample", "adequate", "appetizer", "congestion", "streamline", 
+    "amend", "commission", "retain", "alternative", "compensate", "alike"
+  ];
+  const toeic800c = ["streamline", "quota", "attribute", "forthcoming", "overwhelming", "adequate", "mandatory", "redeem", "apprenticeship", "culinary", "applause", "discard", "overlook", "proceeds", "aboard", "adhere", "commission", "specification", "convince", "specify", "disclosure", "projection", "outlay", "prohibit", "expedite", "mentor", "offset", "remodel", "nursery", "resolve", "dismantle", "chronicle", "turnout", "bulk", "resume", "inlay", "obstacle", "retrieve", "compliance", "undertake", "endeavor", "unload", "adopt", "adapt", "abandon", "congestion", "prestigious", "acclaim", "deliberate", "designate", "attest", "stroll", "amend", "praise", "nominate", "retain", "slate", "novice", "alternative", "admire", "gratitude", "compile", "duplicate", "affiliate", "tailor", "aspire", "tutor", "alike", "compensate", "periodic", "disturb", "candid", "anonymous", "assure", "thoroughly", "imprint", "disrupt", "afterward", "originate", "versatile", "bypass", "awning", "ample", "contrast", "fragile", "coin", "promptly", "unconventional", "conditional", "accountable", "landmark", "steep", "await", "intermittent", "pursue", "exposition", "cozy", "overhaul", "rundown", "roundup"];
+  
+  const toeic900 = ["abash", "abhor", "abide", "abysmal", "acupuncture", "adjourn", "afflict", "affront", "aggrieved", "algae", "allude", "amble", "annihilate", "anorexia", "bleak", "blunder", "bounty", "brass", "bulimia", "burgeoning", "bustle", "canopy", "capricious", "caricature", "chagrin", "chalice", "decease", "deciduous", "defamation", "default", "delve", "desolate", "engrossed", "entail", "envisage", "excrete", "exodus", "feign", "fiasco", "flamboyant", "fleet", "flunk", "forbear", "garnish", "grandiose", "gratifying", "graze", "growl", "haughty", "haze", "hermit", "hindsight", "hinge", "hoax", "ignoble", "imposing", "inept", "infatuated", "insurrection", "juxtapose", "kudos", "latch", "latitude", "mar", "mnemonic", "morbid", "nimbus", "nirvana", "obliterate", "omnivorous", "optical", "outlandish", "pandemonium", "perturb", "placid", "presumptuous", "privy", "prodigal", "profusely", "pseudonym", "puke", "ramshackle", "ransack", "recoil", "redeem", "replenish", "rudimentary", "satirical", "savory", "savvy", "scaffold", "shun", "singular", "tattered", "trample", "upright", "venerate", "vermin", "viable", "wrath"];
+
+  const toeic500 = Array.from(new Set([...toeic400c, ...toeic500b, ...toeic500c])).map(a => a.toLowerCase()).sort((a,b) => (a < b) ? -1 : 1);
+  const toeic600 = Array.from(new Set([...toeic600a, ...toeic600b, ...toeic600c])).map(a => a.toLowerCase()).sort((a,b) => (a < b) ? -1 : 1);
+  const toeic700 = Array.from(new Set([...toeic700a, ...toeic700b, ...toeic700c])).map(a => a.toLowerCase()).sort((a,b) => (a < b) ? -1 : 1);
+  const toeic800 = Array.from(new Set([...toeic800a, ...toeic800b, ...toeic800c, ...toeic900])).map(a => a.toLowerCase()).sort((a,b) => (a < b) ? -1 : 1);
+
+console.log(toeic500.length, toeic600.length, toeic700.length, toeic800.length);
+console.log(JSON.stringify(toeic700));
+// console.log(toeic400c, toeic500b, toeic500c, toeic600a, toeic600b, toeic600c, toeic700a, toeic700b, toeic700c, toeic800a, toeic800b, toeic800c, toeic900);  
