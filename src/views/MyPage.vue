fb51c29161dacf4a0e3ed47529f820ec35b5a3e5
snakajima <satoshi.nakajima@gmail.com>
2024-06-25 13:42:35 -0700

    icons and etc.

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="width:512px; height:512px; background:linear-gradient(135deg, rgba(34,193,195,1) 0%, rgba(148,190,116,1) 50%, rgba(253,187,45,1) 100%)">
-    <span style="color:#fff; margin-top: 40px; font-size: 420px" class="material-symbols-outlined">
+  <div style="width:514px; height:514px; background:linear-gradient(135deg, rgba(34,193,195,1) 0%, rgba(148,190,116,1) 50%, rgba(253,187,45,1) 100%)">
+    <span style="color:#fff; margin-top: 52px; font-size: 420px" class="material-symbols-outlined">
 dictionary
     </span>
   </div>
