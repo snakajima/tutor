61f807f05acb31dc65fec40c1673ab28b6eadfc6
snakajima <satoshi.nakajima@gmail.com>
2024-06-24 19:56:11 -0700

    BookView file

---
+++ xcode/aitango/BookView.swift
@@ -0,0 +1,37 @@
+//
+//  BookView.swift
+//  aitango
+//
+//  Created by SATOSHI NAKAJIMA on 6/24/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct BookView: View {
+    private let book: Book
+    @State private var filterLevel = Level.none
+    
+    var body: some View {
+        NavigationLink {
+            VStack {
+                Picker("Level", selection: $filterLevel) {
+                    ForEach(Level.allCases) { level in
+                        Text(level.rawValue)
+                    }
+                }.pickerStyle(.segmented)
+                List {
+                    ForEach(book.words, id: \.self) { word in
+                        WordLinkView(word: word, bookId: book.id)
+                    }
+                }
+            }.navigationTitle(book.title)
+        } label: {
+            Text(book.title)
+        }
+    }
+    
+    init(book: Book) {
+        self.book = book
+    }
+}
--- xcode/aitango/ContentView.swift
+++ xcode/aitango/ContentView.swift
@@ -12,34 +12,6 @@ import SwiftData
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-struct BookView: View {
-    private let book: Book
-    @State private var filterLevel = Level.none
-    
-    var body: some View {
-        NavigationLink {
-            VStack {
-                Picker("Level", selection: $filterLevel) {
-                    ForEach(Level.allCases) { level in
-                        Text(level.rawValue)
-                    }
-                }.pickerStyle(.segmented)
-                List {
-                    ForEach(book.words, id: \.self) { word in
-                        WordLinkView(word: word, bookId: book.id)
-                    }
-                }
-            }.navigationTitle(book.title)
-        } label: {
-            Text(book.title)
-        }
-    }
-    
-    init(book: Book) {
-        self.book = book
-    }
-}
-
 struct ContentView: View {
     
     @Environment(\.modelContext) private var modelContext
