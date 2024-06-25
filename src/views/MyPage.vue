0ef0b2336f20518e609cd0ca5a026c6a102a1f9f
snakajima <satoshi.nakajima@gmail.com>
2024-06-25 13:00:09 -0700

    logo

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>user</div>
+  <div style="width:512px; height:512px; background:linear-gradient(135deg, rgba(34,193,195,1) 0%, rgba(148,190,116,1) 50%, rgba(253,187,45,1) 100%)">
+    <span style="color:#fff; margin-top: 40px; font-size: 420px" class="material-symbols-outlined">
+dictionary
+    </span>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { requireLogin } from "@/utils/utils";
+//import { requireLogin } from "@/utils/utils";
 
 export default defineComponent({
   setup() {
-    requireLogin("/account");
+    //requireLogin("/account");
 
     return {};
   },
